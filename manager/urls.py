5c928ffdbe75eaec42fb7970acf177f4b2e736ac
taejukim <xownsla@gmail.com>
2016-09-29 20:31:41 +0900

    add the detail view, modify, delete function

--- manager/urls.py
+++ manager/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'manager'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<id>.+)/$', views.detail, name='detail'),
+    url(r'^detailview/(?P<id>.+)/$', views.detail, name='detail'),
+    url(r'^modify/(?P<id>./d+)/$', views.modify, name='modify'),
+    url(r'^delete/(?P<id>./d+)/$', views.delete, name='delete'),
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import ScriptlistModel, CommentModel
 from .forms import ScriptlistForm, CommentForm
 
@@ -9,59 +9,59 @@ def index(request):
 
     if request.method == 'POST':
         forms = ScriptlistForm(request.POST, request.FILES)
+
         if forms.is_valid():
-            newsrc = ScriptlistModel(
-                            scrfile=request.FILES['scrfile'],
-                            author=request.POST['author'],
-                            tcid=request.POST['tcid'],
-                            detail=request.POST['detail']
-                            )
-            newsrc.save()
+            newpost = ScriptlistModel(
+                author=forms.cleaned_data['author'],
+                tcid=forms.cleaned_data['tcid'],
+                detail=forms.cleaned_data['detail'],
+                scrfile=request.FILES['scrfile']
+            )
+            newpost.save()
 
             return HttpResponseRedirect(reverse('manager:index'))
     else:
-        forms = ScriptlistForm
 
-    scripts = ScriptlistModel.objects.all()
+        forms = ScriptlistForm()
+
+    scripts = ScriptlistModel.objects.order_by('-now')
 
     return render(request, 'manager/index.html',{'scripts':scripts, 'forms' : forms})
+
+
 
 def detail(request, id):
-    '''
-    id = ScriptlistModel.objects.filter(id =id )
-    context = {'id': id}
-    #scripts = ScriptlistModel.objects.filter(id = id)
-    #script = {'author':scripts, 'scrfile' : scripts, 'tcid' : scripts, 'detail' : scripts, 'now' : scripts}
+
+    scripts = ScriptlistModel.objects.get(id=id)
+
     if request.method == 'POST':
-        forms = CommentForm(request.POST)
-        if forms.is_valid():
-            newcom = CommentModel(
-                            comments=request.POST['comments'],
-                            writer=request.POST['writer'],
-                            )
-            newcom.save()
+        commentform = CommentForm(request.POST)
 
-            return HttpResponseRedirect(reverse('manager:detail'))
+        if commentform.is_valid():
+            newcomment = CommentModel(
+                script=ScriptlistModel.objects.get(id=id),
+                writer=commentform.cleaned_data['writer'],
+                comments=commentform.cleaned_data['comments']
+            )
+            newcomment.save()
+            return
     else:
-        forms = CommentForm()
-'''
-    comments = CommentModel.objects.all()
 
-    return render(request, 'manager/view.html' comments)
+        commentform = CommentForm()
 
-"""
+    comments = CommentModel.objects.order_by('-time')
+
+    return render(request, 'manager/view.html', {'scripts':scripts,'commentform':commentform, 'comments': comments})
+
+
+def modify(request, id):
+
+    return render(request, 'manager/modify.html', {'id': id})
+
+def delete(request, id):
+
+    posts = ScriptlistModel.objects.get(id=id)
     if request.method == 'POST':
-        contents = ScriptlistForm(request.POST)
-        files = ScriptlistForm(request.FILES)
-        if contents.is_valid():
-            contents.save()
-            files.save()
-
-            return HttpResponseRedirect(reverse('manager:index'))
-    else:
-        files = ScriptlistForm()
-
-    scripts = ScriptlistModel.objects.all()
-
-    return render(request, 'manager/index.html',{'scripts':scripts, 'forms' : forms})
-"""
+        posts.delete()
+        return redirect('manager:detail', id)
+    return render(request, 'manager/view.html', {'posts':posts})
